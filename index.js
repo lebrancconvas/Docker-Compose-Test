e741d360e81cabf0aecacc203d05a0a940600814
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-23 15:00:41 +0700

    :sparkles: Add Run Location.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.set('visits', 0)
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
-        res.send(`Number of Visits is ${visits}`)
+        res.send(`Number of Visits is ${visits}\nThis Website is running on Docker ENV.`)
         client.set('visits', parseInt(visits) + 1)
     })
 })
