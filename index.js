b568dcd8250cd0364e91f4237686aadd6834f327
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-23 14:44:43 +0700

    :tada: Init.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const redis = require('redis')
+const app = express()
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379,
+})
+const PORT = 8091
+
+client.set('visits', 0)
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send(`Number of Visits is ${visits}`)
+        client.set('visits', parseInt(visits) + 1)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Port: ${PORT} is opening.`)
+})
